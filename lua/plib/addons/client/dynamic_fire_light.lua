10270dfca19fe19991993dd56cee7d29724bcd00
PrikolMen <olegis019@gmail.com>
2023-01-19 00:22:37 +0400

    Rebuild

--- lua/plib/addons/client/dynamic_fire_light.lua
+++ lua/plib/addons/client/dynamic_fire_light.lua
@@ -1,32 +1,35 @@
 plib.Require( 'dynamic_light' )
 
+local CreateDynamicLight = CreateDynamicLight
+local hook_Add = hook.Add
+local IsValid = IsValid
+local ipairs = ipairs
+
+local function isFireGlowed( ent )
+	return ent:IsOnFire() or ent:GetClass() == 'env_fire'
+end
+
 timer.Create('PLib - Dynamic Fire Light', 0.25, 0, function()
 	for _, ent in ipairs( ents.GetAll() ) do
-		if ent:IsOnFire() or (ent:GetClass() == 'env_fire') then
+		if isFireGlowed( ent ) then
 			local light = ent.DynamicFireLight
 			if IsValid( light ) then
 				light:SetPos( ent:LocalToWorld( ent:OBBCenter() ) )
-			else
-				local new = CreateDynamicLight( ent:EntIndex() )
-				if IsValid( new ) then
-					ent.DynamicFireLight = new
-					new:SetColorUnpacked( 255, 100, 0 )
-					new:SetBrightness( 1 )
-					new:SetSize( 128 )
-				end
+				return
 			end
-		else
-			local light = ent.DynamicFireLight
-			if IsValid( light ) then
-				light:Remove()
+
+			local new = CreateDynamicLight()
+			if IsValid( new ) then
+				ent.DynamicFireLight = new
+				new:SetColorUnpacked( 255, 100, 0 )
+				new:SetBrightness( 1 )
+				new:SetSize( 128 )
+
+				hook_Add('Think', new, function( self )
+					if IsValid( ent ) and isFireGlowed( ent ) then return end
+					self:Remove()
+				end)
 			end
 		end
-	end
-end)
-
-hook.Add('EntityRemoved', 'PLib - Dynamic Fire Light', function( ent )
-	local light = ent.DynamicFireLight
-	if IsValid( light ) then
-		light:Remove()
 	end
 end)
